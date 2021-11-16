3ae598b785b546f0511e3818226a4eb0aab6dab5
awd86 <awd86@yahoo.com>
2021-11-16 12:40:28 -0800

    Not quite working

--- convert_CSV2YOLO.py
+++ convert_CSV2YOLO.py
@@ -11,20 +11,20 @@ from tqdm import tqdm
 #     # break
 # print(len(classes_names))
 
-classes_coded = [1,2,3,4,5,6,7,8,9,10,11]
+classes_coded = [str(x) for x in list(range(1,12))]
 classes_names = ['biker', 'car', 'pedestrian', 'trafficLight', 'trafficLight-Green', 'trafficLight-GreenLeft', 'trafficLight-Red', 'trafficLight-RedLeft', 'trafficLight-Yellow', 'trafficLight-YellowLeft', 'truck']
 
-print(classes_names.index(1))
+print(classes_coded)
 
 # 601 classes, not 600...
 
 input_file = csv.DictReader(open("./data_given/labels_train.csv"))
 
 for line in tqdm(list(input_file)):
-    print(line)
-    print(line['class_id'],classes_coded.index(line['class_id']))
+    # print(line)
+    # print(line['class_id'],classes_coded.index(line['class_id']))
     with open('./data_given/train/labels/%s.txt'%line['frame'],'w') as f:
-        f.write(','.join([str(classes_coded.index(line['class_id'])),line['XMin'],line['YMin'],str(float(line['XMax'])-float(line['XMin'])),str(float(line['XMax'])-float(line['YMin']))])+'\n')
+        f.write(' '.join([str(classes_coded.index(line['class_id'])),line['xmin'],line['ymin'],str(float(line['xmax'])-float(line['xmin'])),str(float(line['ymax'])-float(line['ymin']))])+'\n')
     # break
 
 input_file = csv.DictReader(open("./data_given/labels_trainval.csv"))
@@ -33,5 +33,5 @@ for line in tqdm(list(input_file)):
     # print(line)
     # print(line['LabelName'],classes_coded.index(line['LabelName']))
     with open('./data_given/test/labels/%s.txt'%line['frame'],'w') as f:
-        f.write(','.join([str(classes_coded.index(line['class_id'])),line['XMin'],line['YMin'],str(float(line['XMax'])-float(line['XMin'])),str(float(line['XMax'])-float(line['YMin']))])+'\n')
+        f.write(' '.join([str(classes_coded.index(line['class_id'])),line['xmin'],line['ymin'],str(float(line['xmax'])-float(line['xmin'])),str(float(line['ymax'])-float(line['ymin']))])+'\n')
     # break
