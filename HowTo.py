834913a3b35e781d40e3b93073a11c5621afc76d
awd86 <awd86@yahoo.com>
2021-11-17 14:51:07 -0800

    given data is smaller, dataset_sub written

--- HowTo.py
+++ HowTo.py
@@ -60,7 +60,8 @@ start = time.time()
 
 # copy/paste this into the terminal  # TODO find out how to execute this from a *.py
 #!python3 train.py --img 416 --rect --batch 8 --epochs 10 --data ./data_given/data.yaml --cfg ./models/customCAR_yolov5l.yaml --weights '' --name yolov5l_results  --cache
-!python3 -m torch.distributed.launch --nproc_per_node 2 train.py --img 1920 --rect --batch 2 --epochs 10 --data ./data_car/data.yaml --cfg ./models/customCAR_yolov5l.yaml --weights '' --name yolov5l_results  --cache
+#!python3 -m torch.distributed.launch --nproc_per_node 2 train.py --img 1920 --rect --batch 2 --epochs 10 --data ./data_car/data.yaml --cfg ./models/customCAR_yolov5l.yaml --weights '' --name yolov5l_results  --cache
+!python3 train.py --img 480 --rect --batch 2 --epochs 10 --data ./data_car_sub/data.yaml --cfg ./models/customCAR_yolov5m.yaml --weights '' --name yolov5m_results  --cache
 
 
 # import train
--- dataset_sub.py
+++ dataset_sub.py
@@ -6,49 +6,54 @@
 
 import csv
 import os  # for mkdir
-from tqdm import tqdm
+import random
+import shutil
+import glob
+
 
 # desired Training Dataset Size
 set_size = 5000
 
 ##### Define the given data structure #####
-source_dir = "./data_given"
+source_dir = "./data_given/"
+source_img_dir = "./data_given/test/images/"
+source_txt_dir = "./data_given/test/labels/"
+dest_dir = "./data_car_sub/"
 order = ['train', 'test', 'valid']
-
-label_file = {
-    'train': "./data_given/labels_train.csv",
-    'valid': "./data_given/labels_val.csv",
-}
 
 
 ##### Check for Directories and make if required #####
+if not os.path.exists(f'{dest_dir}/'):
+    os.mkdir(os.path.join(dest_dir))
+
 for mode in order:
-    if not os.path.exists(f'{source_dir}/{mode}/'):
-        os.mkdir(os.path.join(source_dir,mode))
-        os.mkdir(os.path.join(source_dir,mode,'images'))  # top directory implies that images/ and labels/ also exist
-        os.mkdir(os.path.join(source_dir,mode,'labels'))
+    if not os.path.exists(f'{dest_dir}/{mode}/'):
+        os.mkdir(os.path.join(dest_dir,mode))
+        os.mkdir(os.path.join(dest_dir,mode,'images'))  # top directory implies that images/ and labels/ also exist
+        os.mkdir(os.path.join(dest_dir,mode,'labels'))
 
 
-##### Manually Defining Categories #####
-classes_coded = [str(x) for x in list(range(1,12))]  # manually assign the numbers 1-11 as strings
-classes_names = ['biker', 'car', 'pedestrian', 'trafficLight', 'trafficLight-Green', 'trafficLight-GreenLeft', 'trafficLight-Red', 'trafficLight-RedLeft', 'trafficLight-Yellow', 'trafficLight-YellowLeft', 'truck']
-# classes_coded = []
-#classes_names = []
-#
-# for l in csv.reader(open('./data_given/labels_train.csv')):
-#     # print(l)
-#     classes_coded.append(l[0])
-#     classes_names.append(l[1])
-#     # break
-# print(len(classes_names))
-print(classes_names)
-
-real_set_size = 1.4*set_size
-
+##### Make a list of all image titles in the folder #####
+all_images = os.listdir(path=f'{source_img_dir}')  # ["[image number].jpg",...]
+#print(all_images)
 
 
 ####### Reframe Train and Test Data ######
-for i in real_set_size:  # makes test and valid at 20% size each
+real_set_size = 1.4*set_size
+if real_set_size > len(all_images):
+    print(f"Requested set size of {set_size} doesn't allow 20% test and 20% validation. Only {len(all_images)} available.")
+    print(f"Resetting to maximum set size of {len(all_images)*0.6}")
+    set_size = len(all_images)*0.6
+    real_set_size = len(all_images)
+
+# randomize the sample set
+shuf_set = list(range(real_set_size))
+random.shuffle(shuf_set)
+
+
+##### Copy/Paste Image and Label Files #####
+for i in range(real_set_size):  # makes test and valid at 20% size each
+    n = shuf_set[i]  # random sample
 
     if i <= set_size:
         mode = order[0]  # 'train'
@@ -57,24 +62,13 @@ for i in real_set_size:  # makes test and valid at 20% size each
     else:
         mode = order[2]  # 'valid'
 
-    shutil.copyfile()
+    shutil.copyfile(f"{source_img_dir}/{all_images[n]}", f"{dest_dir}/{mode}/images/{all_images[n]}")  # image
+    # print(f'{all_images[n]} has been moved')
+
+    for label_file in glob.glob(f'{source_txt_dir}/{all_images[n]}.*'):
+        #print(f'{label_file} has been moved')
+        shutil.copy(label_file, f"{dest_dir}/{mode}/labels/")
 
 
-    input_file = csv.DictReader(open(label_file[mode]))
-
-    for line in tqdm(list(input_file)):
-        # print(line)
-        # print(line['LabelName'],classes_coded.index(line['LabelName']))
-
-        # open/make a new txt file for the given image
-        with open(f"./data_given/{mode}/labels/{line['frame']}.txt",'w') as labels:
-            # format from https://medium.com/nerd-for-tech/day-86-dl-custom-object-detector-setup-yolov5-4f5539dd7e9a
-            labels.write(' '.join([str(classes_coded.index(line['class_id'])), str(( float(line['xmax'])+float(line['xmin'])) /2 ), str(( float(line['ymax'])+float(line['ymin'])) /2 ),str(float(line['xmax'])-float(line['xmin'])),str(float(line['ymax'])-float(line['ymin']))])+'\n')
-
-        # move the image (if not already done)
-        if not os.path.exists(f"./data_given/{mode}/images/{line['frame']}"):
-            os.rename(f"./data_given/{line['frame']}", f"./data_given/{mode}/images/{line['frame']}")  # CAUTION - this will delete the original
-            #print(f'{line["frame"]} has been moved to {source_dir}/test/images/')
-
-        #print(f'{source_dir}/test/labels/{line["frame"]}.txt has been written')
-        # break
+##### Move the YAML file #####
+shutil.copy(f'{source_dir}/data.yaml',f'{dest_dir}/data.yaml')
