2f2f8d67199d4a5729a20643c9925f5214d84401
awd86 <awd86@yahoo.com>
2021-12-01 19:10:28 -0800

    given data is smaller, dataset_sub written

--- HowTo.py
+++ HowTo.py
@@ -58,7 +58,7 @@ def writetemplate(line, cell):
 import time
 start = time.time()
 
-# copy/paste this into the terminal  # TODO find out how to execute this from a *.py
+# copy/paste this into the terminal  or execute with '!python'
 #!python3 train.py --img 416 --rect --batch 8 --epochs 10 --data ./data_given/data.yaml --cfg ./models/customCAR_yolov5l.yaml --weights '' --name yolov5l_results  --cache
 #!python3 -m torch.distributed.launch --nproc_per_node 2 train.py --img 1920 --rect --batch 2 --epochs 10 --data ./data_car/data.yaml --cfg ./models/customCAR_yolov5l.yaml --weights '' --name yolov5l_results  --cache
 #!python3 train.py --img 512 --batch 16 --epochs 100 --data ./data_car_small_sub/data.yaml --cfg ./models/customCAR_yolov5m.yaml --weights '' --name yolov5m_results  --cache
