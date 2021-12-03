c7978be4664fe73e206e4fdcffdcee5f7c0c7211
awd86 <awd86@yahoo.com>
2021-12-02 21:59:39 -0800

    Goldilocks converter

---
+++ re_classify.py
@@ -0,0 +1,48 @@
+#####################################################
+#   Alex Denton, 3 Dec 2021, AE4824                 #
+#   re_classify uses a lookup table to change class #
+#####################################################
+
+import numpy as np
+import os
+from pathlib import Path
+from vague_classes import convert  # this is the conversion dict
+
+
+def goldilocks(lbl_dir,convert):
+    # 'lbl_dir' is the path to the folder containing the labels
+    # 'convert' is a dict of old=new categoris values BY NUMBER
+
+    # make goldilocks directory
+    src_dir = Path(lbl_dir).parent.absolute()
+    if not os.path.exists(f'{src_dir}/goldilocks_labels'):
+        os.mkdir(f'{src_dir}/goldilocks_labels')
+    dest_dir = f'{src_dir}/goldilocks_labels'
+
+    # collect label file names
+    label_files = []
+    for file in os.listdir(path=f'{lbl_dir}'):  # ["[image number].tif",...]
+        if file.endswith('.txt'):
+            label_files.append(f'{lbl_dir}/{file}')
+
+    for _label in label_files:
+        # load labels
+        try:
+            labels = np.genfromtxt(_label, delimiter=' ')
+        except:
+            labels = []
+        labels = np.atleast_2d(labels)
+        #print(labels)
+
+        # swap values
+        if labels.any():
+            labels[:,0] = [convert[cls] for cls in labels[:,0]]  # key:value lookup
+
+        # save modified labels
+        np.savetxt(f"{dest_dir}/{_label.split('/')[-1]}", np.atleast_2d(labels), delimiter=' ', newline='\n',
+                   encoding=None)
+
+    print(f'\n{len(label_files)} files converted to goldilocks values.')
+
+
+goldilocks('data_xView/labels',convert)
---
+++ vague_classes.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+
+names = ['Fixed-wing Aircraft', 'Small Aircraft', 'Cargo Plane', 'Helicopter', 'Passenger Vehicle', 'Small Car', 'Bus',
+        'Pickup Truck', 'Utility Truck', 'Truck', 'Cargo Truck', 'Truck w/Box', 'Truck Tractor', 'Trailer',
+        'Truck w/Flatbed', 'Truck w/Liquid', 'Crane Truck', 'Railway Vehicle', 'Passenger Car', 'Cargo Car',
+        'Flat Car', 'Tank car', 'Locomotive', 'Maritime Vessel', 'Motorboat', 'Sailboat', 'Tugboat', 'Barge',
+        'Fishing Vessel', 'Ferry', 'Yacht', 'Container Ship', 'Oil Tanker', 'Engineering Vehicle', 'Tower crane',
+        'Container Crane', 'Reach Stacker', 'Straddle Carrier', 'Mobile Crane', 'Dump Truck', 'Haul Truck',
+        'Scraper/Tractor', 'Front loader/Bulldozer', 'Excavator', 'Cement Mixer', 'Ground Grader', 'Hut/Tent', 'Shed',
+        'Building', 'Aircraft Hangar', 'Damaged Building', 'Facility', 'Construction Site', 'Vehicle Lot', 'Helipad',
+        'Storage Tank', 'Shipping container lot', 'Shipping Container', 'Pylon', 'Tower']
+
+# print(names)
+
+'''
+Manually Determine which classes are:
+1. 'too small'    < 7 m
+2. 'just right'   7 <= l <= 15 m
+3. 'too large'    > 15 m
+'''
+
+names2 = [
+         1,
+         1,
+         3,
+         1,
+         1,
+         1,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         1,
+         2,
+         2,
+         2,
+         2,
+         3,
+         1,
+         1,
+         3,
+         3,
+         3,
+         3,
+         3,
+         3,
+         3,
+         2,
+         3,
+         3,
+         3,
+         3,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         2,
+         1,
+         1,
+         3,
+         3,
+         3,
+         3,
+         3,
+         3,
+         3,
+         3,
+         3,
+         2,
+         3,
+         3,
+]
+
+names3 = names2.copy()
+
+for i in range(len(names2)):
+        if names2[i] == 1:
+                names3[i] = 'too small'
+        elif names2[i] == 2:
+                names3[i] = 'just right'
+        else:
+                names3[i] = 'too big'
+
+conversion = np.array([names2,names,names3]).T
+
+# print(conversion)
+#
+# print(names3)
+convert = {}
+for k in range(len(names2)):
+    convert[k]=names2[k]
+
+#print(convert)
